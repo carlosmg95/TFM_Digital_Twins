ddf5d0df43b8f42884a67d50e06102a0ff72a04c
Carlos Moro <carlosmoro95@gmail.com>
2019-03-09 11:02:43 +0100

    Router edit user

--- router/users.js
+++ router/users.js
@@ -14,5 +14,5 @@ module.exports = function(app) {
     // Index
     app.get('/profile*', controllers.session.requireUser)
     app.get('/profile', controllers.users.show, controllers.render('users/profile/data', 'layout-users'))
-    app.get('/profile/:username', controllers.users.show, controllers.render('users/profile/data', 'layout-users'))
+    app.get('/profile/edit', controllers.render('users/profile/edit', 'layout-users'))
 }
